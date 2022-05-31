9f667e106cd6f2d6e175a5d019339a042b39767a
mdechavez5 <marcdechavez@gmail.com>
2022-05-31 16:44:32 -0400

    Migrated Django Model - Dancer

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .models import Dancer
 
 # Create your views here.
 class Home(TemplateView):
@@ -7,3 +8,22 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class DancerList(TemplateView):
+    template_name = "dancer_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        name = self.request.GET.get("name")
+        # If a query exists we will filter by name 
+        if name != None:
+            # .filter is the sql WHERE statement and name__icontains is doing a search for any name that contains the query param
+            context["dancers"] = Dancer.objects.filter(name__icontains=name)
+            # We add a header context that includes the search param
+            context["header"] = f"Searching for {name}"
+        else:
+            context["dancers"] = Dancer.objects.all() # Here we are using the model to query the database for us.
+            # default header for not searching 
+            context["header"] = "Trending Artists"
+        return context
